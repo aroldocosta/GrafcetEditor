a021ed899e7d97b5b6da42aac2fa02a15165b497
Aroldo Costa <aroldocosta@yahoo.com.b>
2025-08-17 14:29:37 -0300

    feat: synchronizing code

--- source/compiler.js
+++ source/compiler.js
@@ -1,3 +1,14 @@
 function compile(steps) {
-    console.log("Steps: ", steps);
+    let stepsStr = "";
+    let length = stepsList.length
+
+    stepsList.forEach((s, index) => {
+        let item = "!M" + s.id; 
+        if(index < length - 1) item += "*";
+        console.log(item)
+        stepsStr += item;
+    });
+    console.log("SM1=" + stepsStr + "+M"+stepsList[0].inputs[0]);
 }
+
+
